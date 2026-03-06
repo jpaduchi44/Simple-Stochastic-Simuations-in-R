93df1c94ade73b82962c34493f9bfbf32be45262
jpaduchi44 <j.paduchi44@gmail.com>
2026-03-06 22:49:21 +0200

    Markov Chain example
    Example of Markov Chain converging to its stationary distribution

--- MCMC and Random Processes/MCMC_script.R
+++ MCMC and Random Processes/MCMC_script.R
@@ -5,9 +5,16 @@ prob_row <- c(0.2,0.4,0.4, 0, 0, 0,
 0, 0.2, 0.2, 0.3, 0.1,0.2, 
 0, 0, 0.3, 0.4, 0.3, 0, 
 0, 0, 0, 0.6, 0, 0.4)
-trans_P <- matrix(prob_row, 
-                  nrow=6, ncol=6, byrow=TRUE)
+
+trans_P <- matrix(prob_row, nrow=6, ncol=6, byrow=TRUE)
+
+e <- eigen(t(trans_P))
+e$values
+pc <- e$vectors[ , 1]
+pc <- pc / sum(pc)
+pc
+
 init_dist <- c(1, 0, 0, 0, 0, 0)
-sim_mat <- simulateDistribution(state_space, trans_P, init_dist, 30, 500)
+sim_mat <- simulateDistribution(state_space, trans_P, init_dist, 200, 1000)
 heatmap(sim_mat,Rowv=NA, Colv=NA)
-sim_mat
+sim_mat[ , 80]
